a5b6c5c7d7d8aa7b0ba7c2d2e516a3acf7854a28
gus-quintini <gus.quintini@gmail.com>
2017-12-17 14:01:06 -0500

    Fixing pics once again

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var sceneL = document.querySelector('.parallaxscene-left');
+var parallaxInstanceL = new Parallax(sceneL);
+var sceneR = document.querySelector('.parallaxscene-right');
+var parallaxInstanceR = new Parallax(sceneR);
