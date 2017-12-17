fff88f1480fd0624e12497a15efe5f3dd173e758
gus-quintini <gus.quintini@gmail.com>
2017-12-17 14:31:18 -0500

    got rid of js

--- js/main.js
+++
@@ -1,4 +0,0 @@
-var sceneL = document.querySelector('.parallaxscene-left');
-var parallaxInstanceL = new Parallax(sceneL);
-var sceneR = document.querySelector('.parallaxscene-right');
-var parallaxInstanceR = new Parallax(sceneR);
